94a4edd447daf085d1a71ae963e5a68a00b3dfa4
bit-ts <qu.bit@qq.com>
2024-01-27 22:13:44 +0800

    element中文化配置

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs' // element 国际化
+import 'dayjs/locale/zh-cn' // element 用的dayjs要设置时区
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(ElementPlus)
+app.use(ElementPlus, { locale: zhCn })
 app.use(router)
 
 app.mount('#app')
