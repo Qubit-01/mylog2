3ca629ea221e62b53d2cca86b26c53d87caaded5
廖世强 <liaoshiqiang@xiaoduotech.com>
2024-03-25 01:26:07 +0800

    exif-js的bug太多了，现在改为exifreader。旧代码先留一段时间

--- src/utils/img.ts
+++ src/utils/img.ts
@@ -1,15 +1,16 @@
-import EXIF from 'exif-js'
+import * as ExifReader from 'exifreader'
+// import EXIF from 'exif-js'
 
 /**
  * 压缩图片
  * raw 6651136 6.35MB
-   95 1500686 1.43MB
-   20 80343 78.45KB
+ * 95 1500686 1.43MB
+ * 20 80343 78.45KB
  * @param file 压缩文件
  * @param rate 压缩率，默认0.2
  * @returns
  */
-export function compressImg(file: File, rate = 0.2): Promise<File> {
+export function compressImg(file: File, rate = 0.3): Promise<File> {
   function dataURLtoFile(dataurl: string, fileName: string) {
     let arr = dataurl.split(','),
       mime = arr[0].match(/:(.*?);/)![1],
@@ -66,9 +67,8 @@ export function compressImg(file: File, rate = 0.2): Promise<File> {
   })
 }
 
-// 文件URL转 File
-// 输入url和文件名，，是异步操作
 /**
+ * 文件URL转 File, 输入url和文件名，，是异步操作
  * 返回File对象
  * @param url 资源统一定位符
  * @param fileName 文件名，不传则从url中获取
@@ -98,34 +98,96 @@ export function getFileByUrl(url: string, fileName?: string): Promise<File> {
  */
 export interface ExifImgFile extends File {
   exifdata?: any
-  iptcdata?: any
 }
 
 /**
- *提取EXIF信息，写入文件属性中，并返回
+ * 提取EXIF信息，写入文件属性中，并返回。如果错误，就写入空对象
+ * 以前的exif-js太垃圾了，全是bug，现在换成了ExifReader
+ * 目前要用到的信息有(这是手机上拍的，直接上传电脑的)
+ * 信息项             exif-js                                     ExifReader.value
+ * DateTimeOriginal  "2024:03:24 14:37:51"                       "2024:03:24 14:37:51" √
+ * DateTime          "2024:03:24 14:37:51"                       "2024:03:24 14:37:51" √
+ * DateTimeDigitized "2024:03:24 14:37:51"                       "2024:03:24 14:37:51" √
+ * GPSLongitude      [Number {121, numerator: 121, denominator: 1},   [[121, 1],[58, 1],[47064000, 10000000]]
+ * 121.967974         Number {58, numerator: 58, denominator: 1},
+ *                    Number {4.7064, numerator: 47064000, denominator: 10000000}]
+ * GPSLatitude       [Number {30, numerator: 30, denominator: 1},   [[30, 1],[53, 1],[17412000, 10000000]]
+ * 30.883817          Number {53, numerator: 53, denominator: 1},
+ *                    Number {1.7412, numerator: 17412000, denominator: 10000000}
+ * @see https://github.com/mattiasw/ExifReader
  * @param file
  * @return Promise<图片EXIF信息>
  */
-export function getExifByFile(file: ExifImgFile): Promise<any> {
-  return new Promise((resolve, reject) => {
-    try {
-      // @ts-ignore
-      window.n = 0 // exif-js 自身的bug修复
-      // @ts-ignore
-      EXIF.getData(file, () => {
-        resolve(EXIF.getAllTags(file))
-      })
-    } catch (e) {
-      reject(e)
-    }
+export function getExif(file: File): Promise<ExifReader.Tags> {
+  // 传入 File 对象或者 URL
+  return ExifReader.load(file, { async: true }).then(tags => {
+    // MakerNote标签可以非常大。如果您正在解析大量文件并保存标记，请删除它以降低内存使用
+    delete tags['MakerNote']
+
+    // 如果想提取缩略图，可以这样使用
+    // if (tags['Thumbnail'] && tags['Thumbnail'].image) {
+    //   const image = document.getElementById('thumbnail')
+    //   image.classList.remove('hidden')
+    //   image.src = 'data:image/jpg;base64,' + tags['Thumbnail'].base64
+    // }
+    ;(file as ExifImgFile).exifdata = tags
+    console.log(tags)
+    return tags
   })
+
+  // @ts-ignore
+  // window.n = 0 // exif-js 自身的bug修复
+  // return new Promise((resolve, reject) => {
+  //   try {
+  //     // 回调没有输入
+  //     EXIF.getData(file, () => {
+  //       resolve(EXIF.getAllTags(file))
+  //     })
+  //   } catch (error) {
+  //     reject(error)
+  //   }
+  // })
+}
+
+/**
+ * 传入 ExifReader 读取的 exif 数据
+ * @param lng 经度
+ * @param lat 纬度
+ * @returns GPS标准坐标
+ */
+export const getLnglatByExif = (
+  lng: [[number, number], [number, number], [number, number]],
+  lat: [[number, number], [number, number], [number, number]]
+): [number, number] => {
+  const lng1 = [
+    lng[0][0] / lng[0][1],
+    lng[1][0] / lng[1][1],
+    lng[2][0] / lng[2][1],
+  ]
+  const lat1 = [
+    lat[0][0] / lat[0][1],
+    lat[1][0] / lat[1][1],
+    lat[2][0] / lat[2][1],
+  ]
+  return [
+    lng1[0] + lng1[1] / 60 + lng1[2] / 3600,
+    lat1[0] + lat1[1] / 60 + lat1[2] / 3600,
+  ]
 }
 
 // 传入URL，返回图片EXIF信息
-export function getExifFromURL(url: string) {
-  return new Promise(() => {
-    getFileByUrl(url).then(file => {
-      return getExifByFile(file)
-    })
+export function getExifByURL(url: string): Promise<ExifReader.Tags> {
+  // 传入 File 对象或者 URL
+  return ExifReader.load(url, { async: true }).then(tags => {
+    // MakerNote标签可以非常大。如果您正在解析大量文件并保存标记，请删除它以降低内存使用
+    delete tags['MakerNote']
+
+    // 如果想提取缩略图，可以这样使用
+    // if (tags['Thumbnail'] && tags['Thumbnail'].image) {
+    //   const image = document.getElementById('thumbnail')
+    //   image.classList.remove('hidden')
+    //   image.src = 'data:image/jpg;base64,' + tags['Thumbnail'].base64
+    // }
+    return tags
   })
 }
