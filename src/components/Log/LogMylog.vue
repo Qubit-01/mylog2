0706aa0083fa466570f634fbbf760a9f8274ec0b
廖世强 <liaoshiqiang@xiaoduotech.com>
2024-03-28 00:49:43 +0800

    公开log按钮

--- src/components/Log/LogMylog.vue
+++ src/components/Log/LogMylog.vue
@@ -2,9 +2,9 @@
   MyLog和普通log有很多不同，但很多功能也要同步加入
  -->
 <script setup lang="ts">
-import type { Log } from '@/types'
-import { delLog } from '@/stores/log'
-import { Delete, Edit, Share } from '@element-plus/icons-vue'
+import type { Log, LogEdit } from '@/types'
+import { delLog, editLog } from '@/stores/log'
+import { Delete, Edit, Share, Promotion } from '@element-plus/icons-vue'
 
 const { log } = defineProps<{ log: Log }>()
 provide('log', log)
@@ -17,6 +17,19 @@ const expand = () => {
 
 // 点击编辑按钮
 const isEdit = ref(false)
+
+// 点击公开按钮
+const toggleType = (log: Log) => {
+  editLog(
+    {
+      id: log.id!,
+      type: log.type === 'public' ? 'log' : 'public',
+    },
+    { files: [] }
+  ).then(count => {
+    console.log('修改了', count, '条记录')
+  })
+}
 </script>
 
 <template>
@@ -52,7 +65,9 @@ const isEdit = ref(false)
     </template>
 
     <div class="tags">
-      <ElTag v-if="log.type === 'public'" size="small" type="warning">公开</ElTag>
+      <ElTag v-if="log.type === 'public'" size="small" type="warning"
+        >公开</ElTag
+      >
       <ElTag v-for="p in log.people" :key="p" size="small">{{ p }}</ElTag>
       <ElTag v-for="t in log.tags" :key="t" size="small" type="success">
         {{ t }}
@@ -90,6 +105,7 @@ const isEdit = ref(false)
 
     <div v-if="isExpand" class="buttons">
       <ElButtonGroup>
+        <ElButton :icon="Promotion" @click.stop="toggleType(log)" />
         <ElButton :icon="Edit" @click.stop="isEdit = !isEdit" />
         <ElButton :icon="Share" />
         <ElButton :icon="Delete" @click.stop="delLog(log)" />
