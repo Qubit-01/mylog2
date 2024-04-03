3f231c6828d5e1a0fb009e40c4095235d5eea084
bit-mac <2928318801@qq.com>
2024-04-03 17:46:54 +0800

    人脉开始

--- src/views/relation/index.vue
+++ src/views/relation/index.vue
@@ -5,9 +5,12 @@
   中文文档：https://ame.cool/pages/a7d858/
  -->
 <script setup lang="ts">
-import vis from "vis-network/dist/vis-network.min.js"
+import vis from 'vis-network/dist/vis-network.min.js'
+import { FullScreen, Refresh, Plus, Minus } from '@element-plus/icons-vue'
 
-// let nodes = [{ id: 0, label: User.name, color: '#daa', font: { size: 30 } }], edges = []
+let network
+let nodes = [{ id: 0, label: User.name, color: '#daa', font: { size: 30 } }],
+  edges = []
 // const groups = reactive(['亲戚', '其他人'])
 const groups = reactive(['亲戚'])
 
@@ -19,33 +22,47 @@ const options = {
   nodes: {
     shape: 'box', //设置节点node样式为矩形
     fixed: false, //节点node固定可移动
-    font: { size: 20, }, //显示字体大小
+    font: { size: 20 }, //显示字体大小
   },
   edges: {
     arrows: { to: true },
   },
   layout: {
-    randomSeed: 5,  // 布局种子，使其每次布局都一样
+    randomSeed: 5, // 布局种子，使其每次布局都一样
   },
-};
+}
 
-onMounted(() => {
-
-})
+onMounted(() => {})
 console.log('🐤', vis)
 </script>
 
 <template>
   <div class="relation-page">
+    <!-- 外层div 用于fixed占位 -->
+    <!-- <div class="out-network model"> -->
     <div class="relation-network" v-m>
-      网络
+      <div id="network"></div>
+      <div class="buttons">
+        <ElButton type="primary" :icon="FullScreen" @click="MaxNetwork()" />
+        <ElButton type="primary" :icon="Refresh" @click="setScale(0)" />
+        <ElButton type="primary" :icon="Plus" @click="setScale(0.1)" />
+        <ElButton type="primary" :icon="Minus" @click="setScale(-0.1)" />
+      </div>
     </div>
+    <!-- </div> -->
+
+    <!-- 查看人员表单 -->
+    <!-- <PeopleComp
+        :selectedPeople="selectedPeople"
+        :groups="groups"
+        :handleRaws="handleRaws"
+        :raws="raws"
+      /> -->
   </div>
 </template>
 
 <style scoped lang="less">
 .relation-page {
-
   .relation-network {
     padding: var(--padding);
     border-radius: var(--border-radius);
